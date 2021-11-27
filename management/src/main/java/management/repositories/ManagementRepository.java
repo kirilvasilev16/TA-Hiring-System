29a7a73a6702b4287c7f464fa866750d3e0e951e
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-11-27 16:12:41 +0100

    Add management repository

--- management/src/main/java/management/repositories/ManagementRepository.java
+++ management/src/main/java/management/repositories/ManagementRepository.java
@@ -1,10 +1,47 @@
 package management.repositories;
 
+import javax.transaction.Transactional;
 import management.entities.Management;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ManagementRepository extends JpaRepository<Management, Long> {
 
+    /**
+     * Update declared hours.
+     *
+     * @param id the id of the management object
+     * @param declaredHours the new declared hours
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE Management m SET m.declaredHours = ?2 WHERE m.id = ?1")
+    void updateDeclaredHours(long id, float declaredHours);
+
+    /**
+     * Update approved hours.
+     *
+     * @param id the id of the management object
+     * @param declaredHours the new declared hours
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE Management m SET m.declaredHours = ?2, "
+            + "m.approvedHours = ?3 WHERE m.id = ?1")
+    void updateApprovedHours(long id, float declaredHours, float approvedHours);
+
+
+    /**
+     * Update student rating.
+     *
+     * @param id the id of the management object
+     * @param rating new rating
+     */
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    @Query(value = "UPDATE Management m SET m.rating = ?2 WHERE m.id = ?1")
+    void updateRating(long id, float rating);
 }
---
+++ management/src/test/java/management/repositories/ManagementRepositoryTest.java
@@ -0,0 +1,66 @@
+package management.repositories;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Optional;
+import management.entities.Management;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class ManagementRepositoryTest {
+
+    @Autowired
+    private transient ManagementRepository managementRepository;
+
+    @BeforeEach
+    void setUp() {
+        assertNotNull(managementRepository);
+        managementRepository.save(new Management(101, 202, 120));
+    }
+
+    @Test
+    void updateDeclaredHours() {
+        assertTrue(managementRepository.findAll().size() > 0);
+        long id = managementRepository.findAll().get(0).getId();
+        Management management = managementRepository.getOne(id);
+        assertEquals(0, management.getDeclaredHours());
+
+        managementRepository.updateDeclaredHours(id, 20);
+
+        management = managementRepository.getOne(id);
+        assertEquals(20, management.getDeclaredHours());
+    }
+
+    @Test
+    void updateApprovedHours() {
+        assertTrue(managementRepository.findAll().size() > 0);
+        long id = managementRepository.findAll().get(0).getId();
+        Management management = managementRepository.getOne(id);
+        assertEquals(0, management.getDeclaredHours());
+        assertEquals(0, management.getApprovedHours());
+
+        managementRepository.updateApprovedHours(id, 40, 30);
+
+        management = managementRepository.getOne(id);
+        assertEquals(40, management.getDeclaredHours());
+        assertEquals(30, management.getApprovedHours());
+    }
+
+    @Test
+    void updateRating() {
+        assertTrue(managementRepository.findAll().size() > 0);
+        long id = managementRepository.findAll().get(0).getId();
+        Management management = managementRepository.getOne(id);
+        assertEquals(0, management.getRating());
+
+        managementRepository.updateRating(id, 5.0f);
+
+        management = managementRepository.getOne(1L);
+        assertEquals(5.0f, management.getRating());
+    }
+}
