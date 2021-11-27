0b7a5a2907c5492e9210af0ba998996c4a5cf737
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-11-27 18:29:28 +0100

    Add management controller endpoints

--- management/src/main/java/management/controllers/ManagementController.java
+++ management/src/main/java/management/controllers/ManagementController.java
@@ -1,12 +1,16 @@
 package management.controllers;
 
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.websocket.server.PathParam;
 import management.entities.Management;
 import management.services.ManagementService;
+//import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController("Management")
 @RequestMapping("management")
@@ -22,8 +26,71 @@ public class ManagementController {
      *
      * @return the list
      */
-    @GetMapping(value = "findAll")
+    @GetMapping("findAll")
     public List<Management> findAll() {
         return managementService.findAll();
     }
+
+    /**
+     * Gets the management that has the passed id.
+     *
+     * @param id the id
+     * @return the management object
+     */
+    @GetMapping("get")
+    public Management getOne(@PathParam("id") long id) {
+        return managementService.getOne(id);
+    }
+
+    /**
+     * Create a new Management object.
+     *
+     * @param courseId id of course
+     * @param studentId id of student
+     * @param amountOfHours amount of contract hours
+     * @return the management object
+     */
+    @PostMapping("create")
+    public Management create(@PathParam("courseId") long courseId,
+                                 @PathParam("studentId") long studentId,
+                                 @PathParam("amountOfHours") float amountOfHours) {
+        return managementService.createManagement(courseId, studentId, amountOfHours);
+    }
+
+    /**
+     * Declare working hours for student for a given course.
+     *
+     * @param id the id of the management object
+     * @param hours hours declared
+     */
+    @PutMapping("declareHours")
+    public void declareHours(@PathParam("id") long id,
+                             @PathParam("hours") long hours) {
+        managementService.declareHours(id, hours);
+    }
+
+    /**
+     * Approve working hours for student for a given course.
+     *
+     * @param id the id of the management object
+     * @param hours hours declared
+     */
+    @PutMapping("approveHours")
+    public void approveHours(@PathParam("id") long id,
+                             @PathParam("hours") long hours) {
+        managementService.approveHours(id, hours);
+    }
+
+    /**
+     * Rate a student for a given course.
+     *
+     * @param id the id of the management object
+     * @param rating new rating
+     */
+    @PutMapping("rate")
+    public void rate(@PathParam("id") long id,
+                             @PathParam("rating") float rating) {
+        managementService.rateStudent(id, rating);
+    }
+
 }
---
+++ management/src/test/java/management/controllers/ManagementControllerTest.java
@@ -0,0 +1,110 @@
+package management.controllers;
+
+import static org.mockito.Mockito.only;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import java.util.List;
+import management.entities.Management;
+import management.services.ManagementService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest
+class ManagementControllerTest {
+
+    @Autowired
+    private transient MockMvc mockMvc;
+    @Autowired
+    private transient ManagementController managementController;
+    @MockBean
+    private transient ManagementService managementService;
+
+    private transient Management management1;
+    private transient Management management2;
+    private transient List<Management> managements;
+    private transient String findAllResult;
+    private transient String findOneResult;
+
+    @BeforeEach
+    void setUp() {
+        management1 = new Management(101, 202, 120);
+        management1.setId(1);
+        management2 = new Management(50, 45, 70);
+        management2.setId(2);
+        managements = new ArrayList<>();
+        managements.add(management1);
+        managements.add(management2);
+
+        findAllResult = "[{\"id\":1,\"courseId\":101,\"studentId\":202,\"amountOfHours\":120.0,"
+                + "\"approvedHours\":0.0,\"declaredHours\":0.0,\"rating\":0.0},"
+                + "{\"id\":2,\"courseId\":50,\"studentId\":45,\"amountOfHours\":70.0,"
+                + "\"approvedHours\":0.0,\"declaredHours\":0.0,\"rating\":0.0}]";
+
+        findOneResult = "{\"id\":1,\"courseId\":101,\"studentId\":202,\"amountOfHours\":120.0,"
+                + "\"approvedHours\":0.0,\"declaredHours\":0.0,\"rating\":0.0}";
+    }
+
+    @Test
+    void findAll() throws Exception {
+        when(managementService.findAll()).thenReturn(managements);
+
+        this.mockMvc.perform(get("/management/findAll"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(findAllResult));
+    }
+
+    @Test
+    void getOne() throws Exception {
+        when(managementService.getOne(1L)).thenReturn(management1);
+
+        this.mockMvc.perform(get("/management/get?id=1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(findOneResult));
+    }
+
+    @Test
+    void create() throws Exception {
+        when(managementService.createManagement(101, 202, 120))
+                .thenReturn(management1);
+
+        this.mockMvc
+                .perform(post("/management/create?courseId=101&studentId=202&amountOfHours=120"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(findOneResult));
+    }
+
+    @Test
+    void declareHours() throws Exception {
+        this.mockMvc
+                .perform(put("/management/declareHours?id=1&hours=10"))
+                .andExpect(status().isOk());
+        verify(managementService, only()).declareHours(1, 10);
+    }
+
+    @Test
+    void approveHours() throws Exception {
+        this.mockMvc
+                .perform(put("/management/approveHours?id=1&hours=20"))
+                .andExpect(status().isOk());
+        verify(managementService, only()).approveHours(1, 20);
+    }
+
+    @Test
+    void rate() throws Exception {
+        this.mockMvc
+                .perform(put("/management/rate?id=1&rating=2.5"))
+                .andExpect(status().isOk());
+        verify(managementService, only()).rateStudent(1, 2.5f);
+    }
+}
