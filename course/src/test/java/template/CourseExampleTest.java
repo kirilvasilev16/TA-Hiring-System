04f83930430c06bb94b03a519af9e30d49d3e998
Jefferson Yeh <C.Yeh-4@student.tudelft.nl>
2021-12-02 23:03:13 +0100

    Fix checkstyle in test

--- course/src/test/java/template/CourseExampleTest.java
+++ course/src/test/java/template/CourseExampleTest.java
@@ -1,17 +1,18 @@
 package template;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import course.entities.Course;
-import org.junit.jupiter.api.Test;
 import java.util.Date;
 import java.util.HashSet;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CourseExampleTest {
 
     @Test
     public void testGettersSetters() {
-        Course c1 = new Course("CSE1000", "TestName", 5,new HashSet<>(), new Date());
+        Course c1 = new Course("CSE1000", "TestName", 5, new HashSet<>(), new Date());
         assertEquals("TestName", c1.getName());
         /*CourseExample exampleTest = new CourseExample("name");
         assertEquals("name", exampleTest.getName());
