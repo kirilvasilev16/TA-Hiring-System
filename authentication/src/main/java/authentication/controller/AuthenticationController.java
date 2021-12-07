cabec8a0244d474bf08203f47161935b425b625d
Efe <e.sozen@student.tudelft.nl>
2021-12-07 13:43:11 +0100

    api gateway configuration and small working demo

--- authentication/src/main/java/authentication/controller/AuthenticationController.java
+++ authentication/src/main/java/authentication/controller/AuthenticationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("Authentication")
-@RequestMapping("auth")
+@RequestMapping("authentication")
 public class AuthenticationController {
     private final transient AuthenticationService authenticationService;
 
