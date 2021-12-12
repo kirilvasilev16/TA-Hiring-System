b3b36ce53d05710dde2bfed51f28a1cb03aec33d
Jefferson Yeh <C.Yeh-4@student.tudelft.nl>
2021-12-12 21:53:48 +0100

    fix pmdTest violation

--- course/src/test/java/course/DatabaseLoaderTest.java
+++ course/src/test/java/course/DatabaseLoaderTest.java
@@ -1,12 +1,11 @@
 package course;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
 import course.entities.Course;
 import course.repositories.CourseRepository;
-import java.util.Calendar;
-import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -15,14 +14,9 @@ class DatabaseLoaderTest {
 
     @Test
     void testDatabaseLoader() {
-
         CourseRepository courseRepository = Mockito.mock(CourseRepository.class);
-        DatabaseLoader dbLoader = new DatabaseLoader(courseRepository);
-
-        Calendar date = new Calendar.Builder().setDate(2021, 11, 7).build();
-        Course course = new Course("CSE5000", "Sample", 5, new HashSet<String>(), date, 2);
-
-        verify(courseRepository, times(1)).save(course);
+        new DatabaseLoader(courseRepository);
+        verify(courseRepository, times(1)).save(any(Course.class));
     }
 
 }
