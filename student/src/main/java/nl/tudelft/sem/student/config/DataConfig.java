390ecc39a1500d78a94dbf71ecd94e0a8b8ace0a
Oisín Hageman <O.Hageman@student.tudelft.nl>
2021-12-15 14:20:10 +0100

    Set up testing package

--- student/src/main/java/nl/tudelft/sem/student/config/DataConfig.java
+++ student/src/main/java/nl/tudelft/sem/student/config/DataConfig.java
@@ -1,6 +1,8 @@
 package nl.tudelft.sem.student.config;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import nl.tudelft.sem.student.entities.Student;
 import nl.tudelft.sem.student.repositories.StudentRepository;
@@ -17,15 +19,19 @@ public class DataConfig {
     @Bean
     CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
         return args -> {
-            Set<String> c = new HashSet<>();
-            c.add("c1");
-            c.add("c2");
-            Set<String> t = new HashSet<>();
-            t.add("t1");
-            t.add("t2");
+            Map<String, Float> passed = new HashMap<>();
+            passed.put("cse1000", 7.0f);
+            passed.put("cse2000", 8.0f);
+            passed.put("cse3000", 6.0f);
+            passed.put("cse4000", 7.0f);
+            Set<String> candidate = new HashSet<>();
+            candidate.add("cse2000");
+            Set<String> ta = new HashSet<>();
+            ta.add("cse4000");
             Student student1 = new Student("ohageman", "Ois");
-            student1.setCandidateCourses(c);
-            student1.setTaCourses(t);
+            student1.setPassedCourses(passed);
+            student1.setCandidateCourses(candidate);
+            student1.setTaCourses(ta);
             studentRepository.save(student1);
         };
     }
