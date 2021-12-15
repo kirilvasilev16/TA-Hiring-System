208509a3194280d8409a27a3a4ad0f2e70446437
Kiril Vasilev <k.v.vasilev-1@student.tudelft.nl>
2021-12-15 19:08:11 +0100

    Add annotation helper for PMD error

---
+++ management/src/main/java/management/controllers/AnnotationHelper.java
@@ -0,0 +1,9 @@
+package management.controllers;
+
+/**
+ * Helper class solely made to be used for @PathParam annotations.
+ */
+public class AnnotationHelper {
+    public static final transient String courseId = "courseId";
+    public static final transient String studentId = "studentId";
+}
