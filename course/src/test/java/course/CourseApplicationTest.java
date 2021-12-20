e2986dac20399790d22620b278b88ce8f96a6454
Jefferson Yeh <C.Yeh-4@student.tudelft.nl>
2021-12-20 15:58:31 +0100

    fix checkstyle violation

--- course/src/test/java/course/CourseApplicationTest.java
+++ course/src/test/java/course/CourseApplicationTest.java
@@ -1,7 +1,8 @@
 package course;
 
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import org.junit.jupiter.api.Test;
 
 class CourseApplicationTest {
 
