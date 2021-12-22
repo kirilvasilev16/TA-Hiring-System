9591d28ca22ead37116ca474523686377e08fe52
Anna Kalandadze <a.g.kalandadze@student.tudelft.nl>
2021-12-22 17:10:15 +0100

    tests for controller

--- lecturer/src/test/java/lecturer/controllers/LecturerControllerTest.java
+++ lecturer/src/test/java/lecturer/controllers/LecturerControllerTest.java
@@ -1,30 +1,36 @@
 package lecturer.controllers;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import lecturer.entities.Course;
 import lecturer.entities.Lecturer;
+import lecturer.entities.Student;
 import lecturer.services.LecturerService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
 
 @SuppressWarnings("PMD")
 @WebMvcTest
 public class LecturerControllerTest {
     @Autowired
     private transient MockMvc mockMvc;
-    @InjectMocks
     private transient LecturerController lecturerController;
     @MockBean
     private transient LecturerService lecturerService;
@@ -35,11 +41,10 @@ public class LecturerControllerTest {
     private final transient ArrayList<Lecturer> lecturers = new ArrayList<>();
     private transient String findAll;
     private transient String findOne;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setUp() {
-        lecturerService = Mockito.mock(LecturerService.class);
-        lecturerController = new LecturerController(lecturerService);
         courses.add("CSE2215");
         courses.add("CSE2315");
         lecturer1 = new Lecturer("1", "name", "email", courses);
@@ -68,40 +73,112 @@ public class LecturerControllerTest {
     }
 
     @Test
-    void findAllTest() {
+    void findAllTest() throws Exception {
         when(lecturerService.findAll()).thenReturn(lecturers);
-        assertEquals(lecturers, lecturerController.findAll());
+        this.mockMvc.perform(get("/lecturer/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(lecturers)));
     }
 
     @Test
-    void getOne() {
+    void getOne() throws Exception {
         when(lecturerService.findLecturerById("2")).thenReturn(lecturer2);
-        assertEquals(lecturer2, lecturerController.getLecturer("2"));
+        this.mockMvc.perform(get("/lecturer/get")
+                .header("netId", "2"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(lecturer2)));
     }
 
     @Test
     void getOwnCourses() throws Exception {
         when(lecturerService.getOwnCourses("1")).thenReturn(courses);
-        assertEquals(courses, lecturerController.getOwnCourses("1"));
+        this.mockMvc.perform(get("/lecturer/courses/getOwnCourses")
+                .header("netId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(courses)));
     }
 
     @Test
     void getSpecificCourse() throws Exception {
         Course course = new Course("CSE2215", new HashSet<>(), 20);
         when(lecturerService.getSpecificCourseOfLecturer("1", "CSE2215")).thenReturn(course);
-        assertEquals(course, lecturerController.getSpecificCourse("1", "CSE2215"));
+        this.mockMvc.perform(get("/lecturer/courses/getSpecificCourse?courseId=CSE2215")
+                .header("netId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(course)));
     }
 
     @Test
-    void getCandidates() {
+    void getCandidates() throws Exception {
         Set<String> l = new HashSet<>();
         when(lecturerService.getCandidateTaList("1", "CSE2215")).thenReturn(l);
-        assertEquals(l, lecturerController.getCandidateTas("1", "CSE2215"));
+        this.mockMvc.perform(get("/lecturer/courses/getCandidateTas?courseId=CSE2215")
+                .header("netId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(l)));
     }
 
     @Test
-    void addCourse() {
+    void addCourse() throws Exception {
         when(lecturerService.addSpecificCourse("1", "CSE2215")).thenReturn(lecturer1);
-        assertEquals(lecturer1, lecturerController.addSpecificCourse("1", "CSE2215"));
+        this.mockMvc.perform(put("/lecturer/courses/addCourse?courseId=CSE2215")
+                .header("netId", "1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(objectMapper.writeValueAsString(lecturer1)));
+    }
+
+    @Test
+    void selectTa() throws Exception {
+        when(lecturerService.chooseTa("1", "CSE2215", "qw", 25)).thenReturn(true);
+        this.mockMvc.perform(put(
+                "/lecturer/courses/selectTa?courseId=CSE2215?studentId=qw&hours=25")
+                .header("netId", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAvg() throws Exception {
+        when(lecturerService.getAverage("1", "CSE2215", "qw")).thenReturn(9f);
+        this.mockMvc.perform(get("/lecturer/getAverageRating?courseId=CSE2215?studentId=qw")
+                .header("netId", "1"))
+                .andExpect(status().isOk());
+    }
+
+    //TODO
+    @Test
+    void getRecommendations() throws Exception {
+        Student student = new Student("2");
+        List<Student> students = new ArrayList<>();
+        students.add(student);
+        when(lecturerService.getRecommendation("1", "CSE2215", "qw")).thenReturn(students);
+        this.mockMvc.perform(get("/lecturer/courses/recommendations?courseId=CSE2215?strategy=qw")
+                .header("netId", "1"))
+                .andExpect(status().isOk());
+        //.andExpect(content().json(objectMapper.writeValueAsString(students)));
+    }
+
+    @Test
+    void getNumber() throws Exception {
+        when(lecturerService.getNumberOfNeededTas("1", "CSE2215")).thenReturn(10);
+        this.mockMvc.perform(get("/lecturer/courses/getSize?courseId=CSE2215")
+                .header("netId", "1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void addLecturer() throws Exception {
+        doNothing().when(lecturerService).addLecturer(lecturer1);
+        this.mockMvc.perform(post("/lecturer/addLecturer")
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(lecturer1)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void rateTa() throws Exception {
+        doNothing().when(lecturerService).rateTa("1", "CSE2215", "st", 10f);
+        this.mockMvc.perform(get("/lecturer/rateTa?courseId=CSE2215&studentId=st&rating=10")
+                .header("netId", "1"))
+                .andExpect(status().isOk());
     }
 }
