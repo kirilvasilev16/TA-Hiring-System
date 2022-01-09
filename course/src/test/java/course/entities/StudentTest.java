a3c55ec5b33f1b066af5e83d2001feae4b95e610
Jefferson Yeh <C.Yeh-4@student.tudelft.nl>
2022-01-09 01:12:00 +0100

    Add missing branch Student class test

--- course/src/test/java/course/entities/StudentTest.java
+++ course/src/test/java/course/entities/StudentTest.java
@@ -1,8 +1,11 @@
 package course.entities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import course.exceptions.CourseNotPassedException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
@@ -19,7 +22,8 @@ class StudentTest {
     @BeforeEach
     void setUp() {
         netId = "student1";
-        passedCourses = Map.of("CSE2100", 9f);
+        passedCourses = new HashMap<>();
+        passedCourses.put("CSE2100", 9f);
         taCourses = new HashSet<>(Arrays.asList("CSE1005-2020"));
         student = new Student(netId, passedCourses, taCourses);
     }
@@ -63,5 +67,27 @@ class StudentTest {
     @Test
     void getHighestGradeAchieved() {
         assertEquals(9f, student.getHighestGradeAchieved("CSE2100-2022"));
+    }
+
+    @Test
+    void getHighestGradeAchievedNull() {
+        String c = "CSE1300-2020";
+        String strip = "CSE1300";
+        student.getPassedCourses().put(strip, null);
+
+        assertThrows(CourseNotPassedException.class, () -> {
+            student.getHighestGradeAchieved(c);
+        });
+    }
+
+    @Test
+    void getHighestGradeAchievedLower() {
+        String c = "CSE1300-2020";
+        String strip = "CSE1300";
+        student.getPassedCourses().put(strip, -1f);
+
+        assertThrows(CourseNotPassedException.class, () -> {
+            student.getHighestGradeAchieved(c);
+        });
     }
 }
